a965d6cf640069caaf1a9a0f897dc6d8244e5735
AyushmanChakraborty <89172555+AyushmanChakraborty@users.noreply.github.com>
2021-12-16 21:50:25 -0800

    sql

---
+++ testimony2.sql
@@ -0,0 +1,23 @@
+USE MAIN:
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("He is the homie", "Varun Singh");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("He is kind, generous, and understanding", "Aditya Rao");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("Mr. Millard is one of the most quirky teachers at AHS. Everyday when I see him, he brightens my day", "Chakshan Kothakota");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("Mr. Millard is an amazing person and very nice. And, he is cute", "Ryan S.");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("Mr. Millard is a very amazing teacher and is goated and funny", "Rohan S.");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("Mr. Millard is very dependable and a hardworking teacher", "Ruhan S.");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("Mr. Millard is amazing and funny", "T.J. Kalapatapu");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("Mr. Millard always makes me smile and laugh and I look forward to his class", "Aditya K");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("He is a good teacher and I enjoy being around him", "Jeffrey Chao");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("Mr. Millard makes me very happy, and I am thankful for all his hard work towards the AHS CS compartment", "Arjun Arora");
+
+INSERT INTO Testimonies (Testimonies, StudentName) values ("Mr. Millard will always be remembered as an og. His skills cannot be matched", "Ayushman Chakraborty");
